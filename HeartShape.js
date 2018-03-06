08352a6d10edcb237bc58da40dfb1fd527aa35c2
Luis Alfredo <luisalfredomtz@hotmail.com>
2018-03-06 00:52:39 -0600

    RN-floating-hearts

---
+++ HeartShape.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Image } from 'react-native'
+import { PropTypes } from 'prop-types'
+
+const HeartShape = ({ color }) => {
+	return <Image source={require('./icon.png')} style={{ tintColor: color }} />
+}
+
+HeartShape.propTypes = {
+	color: PropTypes.string
+}
+
+HeartShape.defaultProps = {
+	color: 'red'
+}
+
+/**
+ * Exports
+ */
+
+export default HeartShape
